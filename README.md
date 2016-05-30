56206d44635bfeab01d6f41d1d5b5e21b45c47c4
HSOFEUP <hugo.soares@fe.up.pt>
2016-05-30 16:49:32 +0100

    Adding files

---
+++ README.md
@@ -0,0 +1,23 @@
+## Synopsis
+
+Automatic vanishing point detection using LMS and optimized method.  
+Canny detector and hough line are used in order to detect the lines.  
+
+## Features 
+
+The system automatically computes the vanishing point in the image.  
+ 
+User can define how manu VP can be detected.  
+
+
+## Videos/Images
+
+![](./Images/snapshot379.png)
+Points.  
+ 
+
+
+## Contributors
+
+Main Developer: HSO   
+Email: hugo(dot)soares(at)fe(dot)up(dot)pt  
---
+++ Source/errorHSO.h
@@ -0,0 +1,28 @@
+#ifndef __ERRORHSO_H__
+#define __ERRORHSO_H__
+
+#include "cv.h"      
+#include "highgui.h" 
+#include "cxcore.h" 
+
+/** This is the data structure passed to the Levenberg-Marquardt procedure */
+struct data_struct
+{
+	cv::Mat &LSS;		// Line segments vectors (3xN)	
+	cv::Mat &Lengths;	// Length of line segments (NxN) (diagonal)
+	cv::Mat &midPoints;	// Mid points (c=(a+b)/2) (3xN)	
+
+	cv::Mat &K;		// Camera calibration matrix (3x3)
+
+	data_struct (cv::Mat &_LSS, cv::Mat &_Lengths, cv::Mat &_midPoints, cv::Mat &_K): 
+		LSS(_LSS), Lengths(_Lengths), midPoints(_midPoints), K(_K)
+	{
+	}
+};
+
+float distanceHSO( cv::Mat &vanishingPoint, cv::Mat &lineSegment, float lengthLineSegment, cv::Mat &midPoint );
+
+void evaluateHSO( const double *param, int m_dat, const void *data, double *fvec, int *info);
+
+
+#endif // __ERRORHSO_H__
